a2b26be6d0f451d2ce7e544a86fadf2db9bcbcb6
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-06-09 16:14:26 -0400

    palindrom

--- recursion.js
+++ recursion.js
@@ -13,9 +13,17 @@ function longest(words, idx = 0, longestSoFar = 0) {
 }
 
 // return a string with every other letter
-
 function everyOther(str, idx = 0, newStr = "") {
   if (idx >= str.length) return newStr;
   newStr += str[idx];
   return everyOther(str, idx + 2, newStr);
+}
+
+// check if a string is a palindrome or not
+function isPalindrome(str, idx = 0) {
+  let leftIdx = idx;
+  let rightIdx = str.length - idx - 1;
+  if (leftIdx >= rightIdx) return true;
+  if (str[leftidx] !== str[rightIdx]) return false;
+  return isPalindrome(str, idx + 1);
 }
