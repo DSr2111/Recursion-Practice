e01a6fa66abd6e9e1d30d8352127ae1c331eb41b
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-06-09 15:11:04 -0400

    longest

--- recursion.js
+++ recursion.js
@@ -4,3 +4,10 @@ function product(nums, idx = 0) {
   if (idx === nums.length) return 1;
   return nums[idx] * product(nums, idx + 1);
 }
+
+// return the length of the logest word in an array of words
+function longest(words, idx = 0, longestSoFar = 0) {
+  if (idx === words.length) return longestSoFar;
+  longestSoFar = Math.max(words[idx].length, longestSoFar);
+  return longest(words, idx + 1, longestSoFar);
+}
